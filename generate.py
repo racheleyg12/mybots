5dc247202eb4061b302b8be7d7cfa55d55d1a185
racheleyg12 <rgoldma1@uvm.edu>
2021-02-14 16:18:48 -0500

    Made word of towers of stacked blocks

--- generate.py
+++ generate.py
@@ -8,10 +8,30 @@ pyrosim.Start_SDF("boxes.sdf")
 # and length, width and height all equal to 1 meter, in box.sdf.
 length, width, height = 1, 1, 1
 # a, b = 100, 200
-x, y, z = 0, 0, 0.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-x, y, z = 1, 0, 1.5
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length, width, height])
+
+
+# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+# x, y, z = 1, 0, 1.5
+# pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length, width, height])
+x, y, z = 0, 0, 0.5 
+for k in range (6):
+	for j in range (6):
+		for i in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+			z += 1
+			length, width, height = length*0.90, width*0.90, height*0.90
+		# Reset
+		length, width, height = 1, 1, 1
+		z = 0.5 
+		# Shift one in the y-direction
+		y += 1	
+	# Reset
+	length, width, height = 1, 1, 1
+	z = 0.5 
+	y = 0
+	# Shift one in the x-direction
+	x += 1	
+
 
 # Finish generate.py by appending
 pyrosim.End()

b59326884a80d8bbe0d700f0d1e5b3a39474fb2b
racheleyg12 <rgoldma1@uvm.edu>
2021-02-14 12:15:49 -0500

    added a floor to the simulation

--- simulate.py
+++ simulate.py
@@ -1,9 +1,13 @@
 import pybullet as p
 #To slow things down
 import time
+import pybullet_data
 #Creates an object, physicsClient, which handles the physics, and draws the results to a Graphical User Interface (GUI).
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+# Adding a floor
+planeId = p.loadURDF("plane.urdf")
 #add gravity 
 p.setGravity(0,0,-9.8)
 #Simulate the box

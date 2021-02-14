c4d6b0785361dccadb600073ccbe67c9ed6615df
racheleyg12 <rgoldma1@uvm.edu>
2021-02-14 15:18:12 -0500

    Chaning Link Size

--- generate.py
+++ generate.py
@@ -6,7 +6,9 @@ pyrosim.Start_SDF("box.sdf")
 
 # Stores a box with initial position x=0, y=0, z=0.5, 
 # and length, width and height all equal to 1 meter, in box.sdf.
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length, width, height = 1, 2, 3
+# a, b = 100, 200
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
 
 # Finish generate.py by appending
 pyrosim.End()

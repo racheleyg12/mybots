7d930fdd59133fa5aeaf3769b93b2033cdb89aac
racheleyg12 <rgoldma1@uvm.edu>
2021-02-15 17:49:48 -0500

    finished Assignment2, a three-link robot

--- generate.py
+++ generate.py
@@ -18,9 +18,11 @@ def Create_Robot():
 	# description of the robot's body in this urdf file
 	pyrosim.Start_URDF("body.urdf")
 
-	pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1, 1, 1])
-	pyrosim.Send_Joint(name = "Torso_Leg", parent= "Torso" , child = "Leg" , type = "revolute", position = "0.5 0.0 1.0")
-	pyrosim.Send_Cube(name="Leg", pos=[0.5,0,0.5] , size=[1, 1, 1])
+	pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[1, 1, 1])
+	pyrosim.Send_Joint(name = "Torso_FrontLeg", parent= "Torso" , child = "FrontLeg" , type = "revolute", position = "2.0 0.0 1.0")
+	pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1, 1, 1])
+	pyrosim.Send_Joint(name = "Torso_BackLeg", parent= "Torso" , child = "BackLeg" , type = "revolute", position = "1.0 0.0 1.0")
+	pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[1, 1, 1])
 
 	pyrosim.End()
 

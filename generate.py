c317d181bfdcb3659708872a4cdea5bf3621cddd
racheleyg12 <rgoldma1@uvm.edu>
2021-02-15 16:00:57 -0500

    added Creat_World function

--- generate.py
+++ generate.py
@@ -1,37 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-# Tell pyrosim where to store information about the world you'd like to create. 
-# This world will currently be called box, because it will only contain a box (links can be spheres, cylinders, or boxes).
-pyrosim.Start_SDF("boxes.sdf")
-
-# Stores a box with initial position x=0, y=0, z=0.5, 
-# and length, width and height all equal to 1 meter, in box.sdf.
-length, width, height = 1, 1, 1
-# a, b = 100, 200
-
-
-# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-# x, y, z = 1, 0, 1.5
-# pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length, width, height])
-x, y, z = 0, 0, 0.5 
-for k in range (6):
-	for j in range (6):
-		for i in range(10):
-			pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-			z += 1
-			length, width, height = length*0.90, width*0.90, height*0.90
-		# Reset
-		length, width, height = 1, 1, 1
-		z = 0.5 
-		# Shift one in the y-direction
-		y += 1	
-	# Reset
+def Create_World():
+	# Tell pyrosim where to store information about the world you'd like to create. 
+	# This world will currently be called box, because it will only contain a box (links can be spheres, cylinders, or boxes).
+	pyrosim.Start_SDF("boxes.sdf")
+	
+	# Stores a box with initial position x=0, y=0, z=0.5, 
+	# and length, width and height all equal to 1 meter, in box.sdf.
 	length, width, height = 1, 1, 1
-	z = 0.5 
-	y = 0
-	# Shift one in the x-direction
-	x += 1	
+	x, y, z = 0, 0, 0.5
+	pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+		
+	# Finish generate.py by appending
+	pyrosim.End()
 
-
-# Finish generate.py by appending
-pyrosim.End()
+Create_World()
